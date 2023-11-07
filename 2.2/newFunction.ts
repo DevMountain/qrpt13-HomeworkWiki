82c382dfa77f4aea7b08c97d2b64fa5701ad2702
Marohn Howard <mars.howard@devmounta.in>
2023-11-06 18:17:38 -0700

    unit 2.2 homework solution

--- 2.2/newFunction.ts
+++ 2.2/newFunction.ts
@@ -1,14 +1,14 @@
 export function myFunction(myNum: number): string {
     if (myNum == 5) {
-        return 'true'
-    } else if(myNum > 5) {
-        return 'false'
-    }else if (){
-
+        return 'true';
+    } else if(myNum < 5 && myNum >= 0) {
+        return 'false';
+    }else if (myNum > 5){
+        return 'big';
     }else {
-        
-    }
-}
+        return 'negative';
+    };
+};
 
 /* 
 EN: Add your own function starting on line 20 
@@ -17,6 +17,18 @@ EN: Add your own function starting on line 20
 !Add expects to the test checking to make sure you are getting back the correct movie.
 */
 
-export function blockBuster() {
-
-}
+export function blockBuster(movie: string): string {
+    if (movie == "Movie One") {
+        return "Free Guy"; 
+    } else if (movie == "Movie Two") {
+        return "Everything, Everywhere, All at Once";
+    } else if (movie == "Movie Three") {
+        return "Surf's Up"; 
+    } else if (movie == "Movie Four") {
+        return "Lilo and Stitch"; 
+    } else if (movie == "Movie Five") {
+        return 'Ex Machina'; 
+    } else {
+        return `I'm sorry we do not carry ${movie}`; 
+    }
+};
